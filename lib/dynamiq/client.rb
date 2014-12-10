a89f00ab6253b778603ae49195e50cd2be26d7c3
StabbyCutyou <sean.kelly@tapjoy.com>
2014-12-10 17:05:09 -0500

    Comments

--- lib/dynamiq/client.rb
+++ lib/dynamiq/client.rb
@@ -9,6 +9,14 @@ module Dynamiq
     end
 
     # Create a Dynamiq topic
+    # @param topic [String] name of the topic
+    # @param opts [Hash] optional parameters
+    # @example
+    #   @rqs = Dynamiq::Client.new('http://example.io', '9999')
+    #   @rqs.create_topic('my_topic')
+    # => 
+    # true
+    #
     def create_topic(topic, opts={})
       begin
         connection.put("/topics/#{topic}")
@@ -20,6 +28,14 @@ module Dynamiq
     end
 
     # Create a Dynamiq queue
+    # @param queue [String] name of the queue
+    # @param opts [Hash] optional parameters
+    # @example
+    #   @rqs = Dynamiq::Client.new('http://example.io', '9999')
+    #   @rqs.create_queue('my_queue')
+    # => 
+    # true
+    #
     def create_queue(queue, opts={})
       begin
         connection.put("/queues/#{queue}")
@@ -31,6 +47,13 @@ module Dynamiq
     end
 
     # Delete a Dynamiq topic
+    # @param topic [String] name of the topic
+    # @example
+    #   @rqs = Dynamiq::Client.new('http://example.io', '9999')
+    #   @rqs.delete_topic('my_topic')
+    # => 
+    # true
+    #
     def delete_topic(topic)
       begin
         connection.delete("/topics/#{topic}")
@@ -41,7 +64,14 @@ module Dynamiq
       end
     end
 
-    # Delete a Dynamiq queueu
+    # Delete a Dynamiq queue
+    # @param queue [String] name of the queue
+    # @example
+    #   @rqs = Dynamiq::Client.new('http://example.io', '9999')
+    #   @rqs.delete_queue('my_queue')
+    # => 
+    # true
+    #
     def delete_queue(queue)
       begin
         connection.delete("/queues/#{queue}")
@@ -53,6 +83,14 @@ module Dynamiq
     end
 
     # Assign a queue to a topic
+    # @param topic [String] name of the topic
+    # @param queue [String] name of the queue
+    # @example
+    #   @rqs = Dynamiq::Client.new('http://example.io', '9999')
+    #   @rqs.assign_queue('my_topic','my_queue')
+    # => 
+    # true
+    #
     def assign_queue(topic, queue)
       begin
         connection.put("/topics/#{topic}/queues/#{queue}")
@@ -64,6 +102,16 @@ module Dynamiq
     end
 
     # Configure a queue
+    # @param queue [String] name of the queue
+    # @param opts [Integer] :visibility_timeout The number of seconds to wait before making a message available again
+    # @param opts [Integer] :min_partitions The minimum number of partitions for a Queue to serve messages from
+    # @param opts [Integer] :max_partitions The maximum number of partitions for a Queue to serve messages from
+    # @example
+    #   @rqs = Dynamiq::Client.new('http://example.io', '9999')
+    #   @rqs.assign_queue('my_topic','my_queue')
+    # => 
+    # true
+    #
     def configure_queue(queue, opts={})
       begin
         connection.patch("/queues/#{queue}", opts)

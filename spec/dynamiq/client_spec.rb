69d64262b0d993cf423d2c0c13fa7e09814d2753
StabbyCutyou <sean.kelly@tapjoy.com>
2015-01-16 11:16:51 -0500

    Adding specs, cleaning up a dumb thing I was doing in them

--- spec/dynamiq/client_spec.rb
+++ spec/dynamiq/client_spec.rb
@@ -7,14 +7,24 @@ describe Dynamiq::Client do
   let (:port) {'9999'}
   let (:conn) {Faraday.new(:url=>"#{url}:#{port}") }
   let (:response) {Faraday::Response.new({:status=>200, :body => '{}'})}
-  subject (:client) {Dynamiq::Client.new(url,port)}
+  let (:connection_timeout) {5}
+  let (:client_options) {{:connection_timeout=>connection_timeout}}
+  subject (:client) {Dynamiq::Client.new(url,port, client_options)}
+  let(:conn) { subject.connection } 
 
   before :each do
-    subject.stub(:connection).and_return(conn)
     ::Dynamiq.logger.stub(:error)
   end
 
-  context 'create_topic' do
+  context '#initialize' do
+    context "when a connection timeout is provided" do
+      it "is set on the underlying Faraday object" do
+        expect(conn.options.timeout).to eq(connection_timeout) 
+      end
+    end
+  end
+
+  context '#create_topic' do
     it 'should PUT the topic name' do
       conn.should_receive(:put).with("/topics/#{topic_name}")
       subject.create_topic(topic_name)
@@ -36,7 +46,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'create_queue' do
+  context '#create_queue' do
     it 'should PUT the queue name' do
       conn.should_receive(:put).with("/queues/#{queue_name}")
       subject.create_queue(queue_name)
@@ -58,7 +68,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'delete_topic' do
+  context '#delete_topic' do
     it 'should DELETE the topic name' do
       conn.should_receive(:delete).with("/topics/#{topic_name}")
       subject.delete_topic(topic_name)
@@ -80,7 +90,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'delete_queue' do
+  context '#delete_queue' do
     it 'should DELETE the queue name' do
       conn.should_receive(:delete).with("/queues/#{queue_name}")
       subject.delete_queue(queue_name)
@@ -102,7 +112,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'subscribe_queue' do
+  context '#subscribe_queue' do
     it 'should PUT the topic and queue names' do
       conn.should_receive(:put).with("/topics/#{topic_name}/queues/#{queue_name}")
       subject.subscribe_queue(topic_name, queue_name)
@@ -124,7 +134,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'configure_queue' do
+  context '#configure_queue' do
     let (:config_data) {
       {
         :visibility_timeout=>10,
@@ -154,7 +164,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'publish' do
+  context '#publish' do
     it 'should PUT message to topic' do
       conn.should_receive(:put).with("/topics/#{topic_name}/message", {:x=>'y'})
       subject.publish(topic_name, {:x=>'y'})
@@ -176,7 +186,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'enqueue' do
+  context '#enqueue' do
     it 'should PUT message to the queue' do
       conn.should_receive(:put).with("/queues/#{queue_name}/message", {:x=>'y'})
       subject.enqueue(queue_name, {:x=>'y'})
@@ -198,7 +208,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'acknowledge' do
+  context '#acknowledge' do
     it 'should DELETE message from queue' do
       conn.should_receive(:delete).with("/queues/q/message/id")
       subject.acknowledge('q', 'id')
@@ -211,7 +221,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'receive' do
+  context '#receive' do
     before (:each) do
       conn.stub(:get).and_return(response)
     end
@@ -235,7 +245,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'queue_details' do
+  context '#queue_details' do
     before (:each) do
       conn.stub(:get).and_return(response)
     end
@@ -259,7 +269,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'known_queues' do
+  context '#known_queues' do
     let (:response) {Faraday::Response.new({:status=>200, :body => '[]'})}
     before (:each) do
       conn.stub(:get).and_return(response)
@@ -284,7 +294,7 @@ describe Dynamiq::Client do
     end
   end
 
-  context 'known_topics' do
+  context '#known_topics' do
     let (:response) {Faraday::Response.new({:status=>200, :body => '[]'})}
     before (:each) do
       conn.stub(:get).and_return(response)
